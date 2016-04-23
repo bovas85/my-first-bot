7a3a1099dfb141b1826db4edcf068860113b968d
Alessandro Giordo <leelium@protonmail.com>
2016-04-23 15:12:28 +0100

    fixed function index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var express = require('express')
-var bodyParser = require('body-parser')
-var request = require('request')
-var app = express()
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
 var token = "<EAAIEfsNb6YkBAPWwlVpiQqq51tfTWOZBj7X7h7AF5RhymUmQOOH0mOHwfNzstp2ce4o5VdNJkPXgRV9uR0eZBsiFpSXHNAeEyUnTddWZB2pZCmfBMLjM2214u3NhkL18iIDt8uuGWNsFQb9oxRKKvorCtYNjLL5iLCBsZCMzRYAZDZD>";
 
 app.set('port', (process.env.PORT || 5000));
@@ -27,7 +27,7 @@ app.get('/webhook/', function (req, res) {
 
 // Spin up the server
 app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
+    console.log('running on port', app.get('port'));
 });
 app.post('/webhook/', function (req, res) {
     messaging_events = req.body.entry[0].messaging;
@@ -36,7 +36,16 @@ app.post('/webhook/', function (req, res) {
         sender = event.sender.id;
         if (event.message && event.message.text) {
             text = event.message.text;
+            if (text === 'Generic') {
+                sendGenericMessage(sender);
+                continue;
+            }
             sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
+        }
+        if (event.postback) {
+            text = JSON.stringify(event.postback);
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+            continue;
         }
     }
     res.sendStatus(200);
@@ -47,6 +56,55 @@ app.post('/webhook/', function (req, res) {
 function sendTextMessage(sender, text) {
     messageData = {
         text:text
+    };
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token:token},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            message: messageData,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending messages: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+}
+
+function sendGenericMessage(sender) {
+    messageData = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "First card",
+                    "subtitle": "Element #1 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+                    "buttons": [{
+                        "type": "web_url",
+                        "url": "https://www.messenger.com",
+                        "title": "web url"
+                    }, {
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for first element in a generic bubble",
+                    }],
+                }, {
+                    "title": "Second card",
+                    "subtitle": "Element #2 of an hscroll",
+                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+                    "buttons": [{
+                        "type": "postback",
+                        "title": "Postback",
+                        "payload": "Payload for second element in a generic bubble",
+                    }],
+                }]
+            }
+        }
     };
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
