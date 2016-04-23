a30dec7ac4c1f7f57070ddd42437a03554ca4d79
Alessandro Giordo <leelium@protonmail.com>
2016-04-23 15:08:10 +0100

    fixed function index.js

--- index.js
+++ index.js
@@ -2,54 +2,52 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var request = require('request')
 var app = express()
+var token = "<EAAIEfsNb6YkBAPWwlVpiQqq51tfTWOZBj7X7h7AF5RhymUmQOOH0mOHwfNzstp2ce4o5VdNJkPXgRV9uR0eZBsiFpSXHNAeEyUnTddWZB2pZCmfBMLjM2214u3NhkL18iIDt8uuGWNsFQb9oxRKKvorCtYNjLL5iLCBsZCMzRYAZDZD>";
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Hello world, I am a chat bot')
-})
+    res.send('Hello world, I am a chat bot');
+});
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token')
-})
+    res.send('Error, wrong token');
+});
 
 // Spin up the server
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'))
-})
+});
 app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
+    messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
         if (event.message && event.message.text) {
-            text = event.message.text
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            text = event.message.text;
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
         }
     }
-    res.sendStatus(200)
-})
-
-var token = "<EAAIEfsNb6YkBAPWwlVpiQqq51tfTWOZBj7X7h7AF5RhymUmQOOH0mOHwfNzstp2ce4o5VdNJkPXgRV9uR0eZBsiFpSXHNAeEyUnTddWZB2pZCmfBMLjM2214u3NhkL18iIDt8uuGWNsFQb9oxRKKvorCtYNjLL5iLCBsZCMzRYAZDZD>"
-
+    res.sendStatus(200);
+});
 
 // functions
 
 function sendTextMessage(sender, text) {
     messageData = {
         text:text
-    }
+    };
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
@@ -60,9 +58,9 @@ function sendTextMessage(sender, text) {
         }
     }, function(error, response, body) {
         if (error) {
-            console.log('Error sending messages: ', error)
+            console.log('Error sending messages: ', error);
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
+            console.log('Error: ', response.body.error);
         }
-    })
+    });
 }
