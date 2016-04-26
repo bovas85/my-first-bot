1ec801baecdaff31b76d8b6e6c9e1736f11778c6
Alessandro Giordo <leelium@protonmail.com>
2016-04-26 22:55:27 +0100

    test

--- index.js
+++ index.js
@@ -30,26 +30,26 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
 });
 app.post('/webhook/', function (req, res) {
-    messaging_events = req.body.entry[0].messaging
+    messaging_events = req.body.entry[0].messaging;
     for (i = 0; i < messaging_events.length; i++) {
-        event = req.body.entry[0].messaging[i]
-        sender = event.sender.id
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
         if (event.message && event.message.text) {
-            text = event.message.text
+            text = event.message.text;
             if (text === 'Generic') {
-                sendGenericMessage(sender)
-                continue
+                sendGenericMessage(sender);
+                continue;
             }
-            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200));
         }
         if (event.postback) {
-            text = JSON.stringify(event.postback)
-            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-            continue
+            text = JSON.stringify(event.postback);
+            sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
+            continue;
         }
     }
-    res.sendStatus(200)
-})
+    res.sendStatus(200);
+});
 
 // functions
 
@@ -105,7 +105,7 @@ function sendGenericMessage(sender) {
                 }]
             }
         }
-    }
+    };
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token:token},
@@ -116,9 +116,9 @@ function sendGenericMessage(sender) {
         }
     }, function(error, response, body) {
         if (error) {
-            console.log('Error sending messages: ', error)
+            console.log('Error sending messages: ', error);
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
+            console.log('Error: ', response.body.error);
         }
-    })
+    });
 }
